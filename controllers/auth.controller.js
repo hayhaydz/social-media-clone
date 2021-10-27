8c832d3bf973c76093f2d91ef3b61fd02d19bb0e
hayhaydz <haydon1900@gmail.com>
2021-10-27 20:00:49 +0100

    Progress: JWT Refresh token storage

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import repository from '../repositories/repoisitory';
+import dao from '../repositories/dao';
 const bcrypt = require('bcrypt');
 
 const { 
@@ -8,8 +9,11 @@ const {
 } = process.env;
 
 const encodeToken = (tokenData) => {
-    // return jwt.sign(tokenData, ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
-    return jwt.sign(tokenData, ACCESS_TOKEN_SECRET);
+    let tokens = [];
+    tokens.push(jwt.sign(tokenData, ACCESS_TOKEN_SECRET, { expiresIn: '60s' }));
+    tokens.push(jwt.sign(tokenData, REFRESH_TOKEN_SECRET, { expiresIn: '7d' }));
+    return tokens;
+    // return jwt.sign(tokenData, ACCESS_TOKEN_SECRET);
 }
 
 const decodeToken = (token) => {
@@ -24,19 +28,21 @@ export const authMiddleware = async (req, res, next) => {
 
     try {
         const decoded = decodeToken(token);
-        const { userId } = decoded;
-        const user = await repository.getUserById(userId);
+        console.log(decoded);
+        const { user_id } = decoded;
+        const user = await repository.getUserById(user_id);
         if(user) {
-            req.userId = userId;
+            req.user_id = user_id;
         }
     } catch (e) {
+        console.log(e);
         return next();
     }
     next();
 }
 
 export const authenticated = (req, res, next) => {
-    if(req.userId) {
+    if(req.user_id) {
         return next();
     }
 
@@ -44,26 +50,57 @@ export const authenticated = (req, res, next) => {
     res.json({ error: 'User is not authenticated' });
 }
 
-const returnInvalidCredentials = (res) => {
+const returnInvalidCredentials = (res, msg) => {
     res.status(401);
-    return res.json({ error: 'Invalid username or password' });
+    return res.json({ error: msg });
+}
+
+export const refresh = async (req, res) => {
+    const refreshToken = req.body.token;
+    if(refreshToken == null) return returnInvalidCredentials(res, 'A token must be provided');
+    // if(!dao.getToken(refreshToken)) return returnInvalidCredentials(res, 'Invalid token provided');
+    let tokenCheck = false;
+    await repository.checkToken(refreshToken, result => {
+        console.log('good')
+        console.log(result);
+        tokenCheck = true;
+    })
+    .catch(err => {
+        console.log('bad');
+        console.log(err);
+    });
+    // console.log(tokenCheck);
+    // jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    //     if(err) return res.sendStatus(403);
+    //     const accessToken = generateAccessToken({ name: user.name });
+    //     res.json({accessToken: accessToken });
+    // })
+    res.json({ msg: 'hello there' });
+    res.status(200);
 }
 
 export const login = async (req, res) => {
     const { username, password } = req.body;
-
     const user = await repository.getUserByUsername(username);
 
     if(!user) {
-        returnInvalidCredentials(res);
+        returnInvalidCredentials(res, 'Invalid username or password');
     }
 
-    bcrypt.compare(password, user.password, (err, result) => {
+    bcrypt.compare(password, user.password, async (err, result) => {
         if(result) {
-            const accessToken = encodeToken({ userId: user.id });
-            return res.json({ accessToken });
+            const tokens = encodeToken({ userId: user.user_id });
+            let date = new Date();
+            date.setDate(date.getDate() + 7);
+            await repository.insertToken(user.user_id, tokens[1], Date.now(), Math.floor(date.getTime() / 1000), result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            return res.json({ accessToken: tokens[0], refreshToken: tokens[1] });
         } else {
-            return returnInvalidCredentials(res);
+            return returnInvalidCredentials(res, 'Invalid username or password');
         }
     });
 }
