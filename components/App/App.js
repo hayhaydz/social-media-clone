37b7ec61737d1bbcdcb2f2e3f40caf0dabd57bf9
hayhaydz <haydon1900@gmail.com>
2021-11-03 14:01:07 +0000

    Global secret key environment variable. Handling frontend login.

--- components/App/App.js
+++ components/App/App.js
@@ -1,56 +1,27 @@
-import react, { useState } from 'react'
-import { getAllUsers, createUser } from '../../services/UserService'
+import react, { useState } from 'react';
+import { loginUser } from '../../services/auth';
 
 const App = () => {
-    const [user, setUser] = useState({});
-    const [users, setUsers] = useState([]);
-    const [numberOfUsers, setNumberOfUsers] = useState(0);
 
-    const creatingUser = (tmpUser) => {
-        createUser(tmpUser)
-          .then(response => {
-            console.log(response);
-            setNumberOfUsers(numberOfUsers + 1);
-        });
-    }
-  
-    const gettingAllUsers = () => {
-      getAllUsers()
-        .then(users => {
-          console.log(users);
-          setUsers(users);
-          setNumberOfUsers(users.length);
-        });
-    }
-
-    const handleForm = () => {
+    const handleForm = (e) => {
+        e.preventDefault();
         let form = document.getElementById('form');
-        let tmpUser = user;
 
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let data = new FormData(form);
-            for (let pair of data.entries()) {
-                tmpUser[pair[0]] = pair[1];
-            }
-
-            setUser({...tmpUser});
-            creatingUser(tmpUser);
-        })
+        let data = new FormData(form);
+        let object = {};
+        data.forEach((value, key) => object[key] = value);
+        loginUser(object);
     }
 
     return (
         <div className="app">
-            <h1>Working between client and server</h1>
-            <form action="#" id="form">
-                <label htmlFor="firstname">First name</label>
-                <input type="text" id="firstname" name="firstname" />
-                <label htmlFor="lastname">Last name</label>
-                <input type="text" id="lastname" name="lastname"/>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" />
-                <button type="submit" onClick={() => handleForm()}>Register</button>
+            <h1>Login to API</h1>
+            <form action="#" id="form" name="form" onSubmit={(e) => handleForm(e)}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" />
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" name="password" type="password" />
+                <button type="submit">Login</button>
             </form>
         </div>
     )
