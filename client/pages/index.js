ee2b4de2f41be3c28d0e48c73aa4cc3a851170cb
hayhaydz <haydon1900@gmail.com>
2021-11-10 13:51:08 +0000

    Client authentication flow

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { App, Layout } from '../components';
+import { getAuth } from '../utils/apiHandler';
+import { auth } from '../utils/auth';
 
 const Home = () => {
+  const token = auth();
+
   return (
     <Layout>
       <App />
