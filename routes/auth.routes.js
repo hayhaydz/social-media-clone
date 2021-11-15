adeaee2eb3e51043f98e47f2dc8edc0cf41b1c2a
hayhaydz <haydon1900@gmail.com>
2021-11-15 13:20:53 +0000

    Complete: Logout process. Work on: Refine auth flow

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
-import { register , login, refresh } from '../controllers/auth.controller';
+import { register , login, logout, refresh } from '../controllers/auth.controller';
 import * as express from 'express';
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 router.post('/refresh', refresh);
 
 module.exports = router;
