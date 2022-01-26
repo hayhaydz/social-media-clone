a3576ef8db87c432d1409357bc624944be41bec7
hayhaydz <haydon1900@gmail.com>
2022-01-26 12:26:55 +0000

    changes

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,25 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: ['@babel/polyfill', './index.js']
-  },
+  entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.bundle.js'
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
-}
+};
