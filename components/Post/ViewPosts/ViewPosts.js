ef59480768c73a47a3c4eab5e7ce20d7311f166b
hayhaydz <haydon1900@gmail.com>
2022-01-26 12:12:03 +0000

    revert

--- components/Post/ViewPosts/ViewPosts.js
+++ components/Post/ViewPosts/ViewPosts.js
@@ -1,23 +1,21 @@
-import { usePagination } from '../../../utils/hooks';
+import useSWR from 'swr';
+import { getAuth } from '../../../utils/apiHandler';
 import Post from '../Post';
 
-const ViewPost = ({ jwt, currentUsersID, BASE_URL}) => {
+const ViewPost = ({ jwt, currentUsersID, url}) => {
+    const fetcher = (url, token) => getAuth(url, token).then((r) => r.json());
+    const { data: response = {}, isLoading, isError } = useSWR([url, jwt], fetcher, {refreshInterval: 1000});
 
-    const { paginatedPosts, isReachedEnd, loadingMore, error, fetchNextPage } = usePagination(BASE_URL, jwt);
-    if(!paginatedPosts) return <div>Loading...</div>
-    if(error) return <div>Fetching posts has failed</div>;
+    if(isLoading) return <div>Loading...</div>
+    if(isError) return <div>Fetching posts has failed</div>;
 
     return (
         <div className="posts flex flex-col items-center">
-            {
-                paginatedPosts?.map((page) => {
-                    return page.data?.map((post, index) => {
-                        return <Post key={index} {...post} currentUsersID={currentUsersID} jwt={jwt} isSingle={false}/>
-                    })
+            {response.data &&
+                response.data.map((post, index) => {
+                    return <Post key={index} {...post} currentUsersID={currentUsersID} jwt={jwt} isSingle={false}/>
                 })
             }
-            {loadingMore && <div>Loading...</div>}
-            {!isReachedEnd && <button onClick={fetchNextPage} className="btn">Load More</button>}
         </div>
     )
 
